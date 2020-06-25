a4c143ea35a759ebdf5f57215209b8d9618ce356
peterchims <peterbozz1996@gmail.com>
2020-06-25 18:51:24 +0100

    Faq changes

--- src/Components/Feedback/faq.js
+++ src/Components/Feedback/faq.js
@@ -1,25 +1,42 @@
 /* eslint-disable class-methods-use-this */
 import React , {Component} from 'react'
-
+import './assets/css/faq.css'
 
 class Faq extends Component{
+  constructor(){
+    super()
+    this.state = {
+      comments:""
+    }
+  }
+
+text = event =>{
+  this.setState({
+    comments:event.target.value
+  })
+}
+
+handleSubmit = event =>{
+  console.log(`Comments Entered by User: ${this.state.comments}`)
+  event.preventDefault();
+}
 
   render(){
     return(
-      <div className="container cintainer-fluid faq-container">
+      <div className="container container-fluid faq-container">
         <div className="p-5 m-5 main-wrapper">
           <h1 className="faq-header">Ask your Questions here?</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <textarea
-            className="form-control"
+            onChange={this.text}
+            value={this.state.comments}
+            className="form-control faq-text"
             placeholder="Write something here..."
             required
-            cols="20"
-            rows="4"
             ></textarea>
             <button className="btn btn-danger">Submit</button>
           </form>
-          <a href="#" className="nav-link">Some Frequently asked questions. </a>
+          <a href="#" className="nav-link">Some Frequently asked questions? </a>
           
         </div>
       </div>
